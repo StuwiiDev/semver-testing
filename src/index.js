869467e20380e018a9db1cdbc3902bfd50f47d58
Stuart.Maskell <stuart.maskell@education.gov.uk>
2024-11-05 16:08:53 +0000

    feat(index): added a protending console log

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log('hello world!');
-console.log('I created a release, woohoo!');
-console.log('another console log')
-console.log('')
+console.log("hello world!");
+console.log("I created a release, woohoo!");
+console.log("another console log");
+console.log("potending");
